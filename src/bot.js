d377f13acfd1ea63800f0e396c8e0aab10eade2a
tckthecreator <dsoouuzavitu@gmail.com>
2021-09-11 22:28:02 -0300

    Pequenas alterações de arquitetura

---
+++ src/bot.js
@@ -0,0 +1,25 @@
+const Twit = require('twit');
+
+function Bot() {
+    const T = new Twit({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY, 
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token: process.env.TWITTER_ACCESS_TOKEN,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
+      
+    /*LISTEN TO TWEETS IN REAL TIME WITH @BOT_TAKETHEHINT MENTIONS*/
+    const mentions = T.stream('statuses/filter', { track: '@bot_takethehint' });
+      
+    mentions.on('tweet', (tweet) => {
+        /*VERIFY IF THE TWEET IS REPLYING SOMEONE. IF NOT, RETURN NOTHING*/
+        if (tweet.in_reply_to_status_id_str === null) return;
+      
+        T.post('statuses/update', { status: `Autor: @${tweet.in_reply_to_screen_name}, 
+Visto por: @${tweet.user.screen_name}`, 
+            attachment_url: `https://twitter.com/${tweet.in_reply_to_screen_name}/status/${tweet.in_reply_to_status_id_str}` 
+        });
+    });
+}
+
+module.exports = Bot;
