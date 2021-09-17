764b66497bf09ad3d6a71081d0e308b2bb1318dc
tckthecreator <dsoouuzavitu@gmail.com>
2021-09-17 17:55:53 -0300

    added like and follow

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ const T = new Twit({
   
 /*LISTEN TO TWEETS IN REAL TIME WITH @BOT_TAKETHEHINT MENTIONS*/
 const mentions = T.stream('statuses/filter', { track: '@bot_takethehint' });
-  
+
+/**WHEN THE MENTION EVENT IS TRIGGER*/  
 mentions.on('tweet', (tweet) => {
+
     /*VERIFY IF THE TWEET IS REPLYING SOMEONE. IF NOT, RETURN NOTHING*/
     if (tweet.in_reply_to_status_id_str === null) return;
+
+    T.post('favorites/create', { id: tweet.id_str });
+    T.post('favorites/create', { id: tweet.in_reply_to_status_id_str });
+
+    T.post('friendships/create', { screen_name: tweet.in_reply_to_screen_name});
   
-    T.post('statuses/update', { status: `Autor: @${tweet.in_reply_to_screen_name}, 
-Visto por: @${tweet.user.screen_name}`, 
-        attachment_url: `https://twitter.com/${tweet.in_reply_to_screen_name}/status/${tweet.in_reply_to_status_id_str}` 
+    T.post('statuses/update', { status: `From: @${tweet.in_reply_to_screen_name
+        }`, attachment_url: `https://twitter.com/${tweet.in_reply_to_screen_name}/status/${tweet.in_reply_to_status_id_str}` 
     });
 });
-
-console.log('Bot iniciado');
-    
-
-
 
